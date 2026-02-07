b33e5e3fcd590af9ecae5255a741122260ef4f5b
Marco Aurélio <marcoaurelio_db@hotmail.com>
2026-02-07 20:37:24 -0300

    feat: add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,45 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  mount Rswag::Ui::Engine => "/api-docs"
+  mount Rswag::Api::Engine => "/api-docs"
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  mount_devise_token_auth_for "User", at: "api/v1/auth"
+
+  namespace :api do
+    namespace :v1 do
+      get "health", to: "health#check"
+
+      resources :companies
+      resources :units
+      resources :rooms
+      resources :networks
+      resources :departments
+      resources :teams
+      resources :groups
+      resources :roles do
+        resources :permissions
+      end
+      resources :users
+      resources :ticket_categories do
+        resources :slas
+        resources :ticket_custom_fields
+      end
+      resources :ticket_statuses
+      resources :ticket_priorities
+      resources :tickets do
+        resources :comments
+        resources :tasks, controller: "ticket_tasks"
+        resources :links, controller: "ticket_links"
+        resources :histories, controller: "ticket_histories", only: [ :index ]
+      end
+      resources :ticket_schedules
+      resources :item_categories
+      resources :items do
+        resources :stock_records
+      end
+      resources :devices
+      resources :loans
+      resources :satisfaction_surveys
+    end
+  end
 end
